75189eaad11aaf87877084dcae67691f0baac4a8
ASood11 <64813241+ASood11@users.noreply.github.com>
2023-03-05 08:12:34 +0530

    Update mdmreboot.ps1

--- mdmreboot.ps1
+++ mdmreboot.ps1
@@ -1,1 +1,1 @@
-Invoke-CimMethod - InputObject (Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_Reboot" -Filter "ParentID='./Vendor/MSFT' and InstanceID='Reboot'") -MethodName "RebootNowMethod"
+Invoke-CimMethod -InputObject (Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_Reboot" -Filter "ParentID='./Vendor/MSFT' and InstanceID='Reboot'") -MethodName "RebootNowMethod"
